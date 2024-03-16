6cc4908160434b1e11e583d65a264a7fb87ae0ae
Camilo Galvis <camilo@dsinno.io>
2024-03-15 21:36:17 -0500

    adding list queries from trip, user, driver

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI
 from routers.auth_router import auth_router
 from routers.user_router import user_router
 from routers.driver_router import driver_router
+from routers.trip_router import trip_router
 
 app = FastAPI()
 
 app.include_router(auth_router, prefix='/api')
 app.include_router(user_router, prefix='/api')
 app.include_router(driver_router, prefix='/api')
+app.include_router(trip_router, prefix='/api')
